06ad390cb20c949a8aa54d72289f7b6199501f84
Hank <kuanc1784@gmail.com>
2024-06-11 11:37:58 +0800

    7 commits

--- main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def root():
-    return "Hello World!"
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="172.0.0.1",port=8000,reload=True)
